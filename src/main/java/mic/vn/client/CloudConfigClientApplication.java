8c33a9d6404af1206a070367d0b34c31ff554809
Nguyen Huu Truong <truongnh@MIC.VN>
2022-06-14 15:03:32 +0700

    add demo for rabbit

--- src/main/java/mic/vn/client/CloudConfigClientApplication.java
+++ src/main/java/mic/vn/client/CloudConfigClientApplication.java
@@ -1,5 +1,6 @@
 package mic.vn.client;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,5 +12,4 @@ public class CloudConfigClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigClientApplication.class, args);
 	}
-
 }
---
+++ src/main/java/mic/vn/client/controller/Sender.java
@@ -0,0 +1,34 @@
+package mic.vn.client.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Sender {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+
+
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+	@Autowired
+	Binding binding;
+
+	@GetMapping(value = "/send/{msg}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String send(@PathVariable("msg") final String message) {
+		LOGGER.info("Sending message to the queue.");
+		rabbitTemplate.convertAndSend(binding.getExchange(), binding.getRoutingKey(), message);
+		LOGGER.info("Message sent successfully to the queue!!!");
+		return "Great!! your message is sent";
+	}
+}
